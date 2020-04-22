219b252012424d0e1b58c99913ef4542d6591c06
Sander Homan <sander.homan@wordtango.net>
2020-04-22 11:55:00 +0200

    Small cleanup of validator tests

--- test/validator/Tests.fs
+++ test/validator/Tests.fs
@@ -13,20 +13,20 @@ let isEqual a b =
 let ``returns Ok when all types are present`` () =
   let data = System.IO.File.ReadAllText "data/Ok.graphql"
 
-  let result = parse data
-  match result with
-  | Ok ast ->
-    let validateResult = validate ast
-    Assert.True((match validateResult with | Ok () -> true | Error _ -> false), sprintf "Failed: %A" validateResult)
-  | Error x -> Assert.True(false,  sprintf "Failed: %A" x)
+  let result =
+    parse data
+    |> Result.mapError List.singleton
+    |> Result.bind validate
+  
+  Assert.True((match result with | Ok () -> true | Error _ -> false), sprintf "Failed: %A" result)
 
 [<Fact>]
 let ``returns Error when not all types are present`` () =
   let data = System.IO.File.ReadAllText "data/Error.graphql"
 
-  let result = parse data
-  match result with
-  | Ok ast ->
-    let validateResult = validate ast
-    Assert.False((match validateResult with | Ok () -> true | Error _ -> false), sprintf "Failed: %A" validateResult)
-  | Error x -> Assert.True(false,  sprintf "Failed: %A" x)
+  let result =
+    parse data
+    |> Result.mapError List.singleton
+    |> Result.bind validate
+
+  Assert.False((match result with | Ok () -> true | Error _ -> false), sprintf "Failed: %A" result)
